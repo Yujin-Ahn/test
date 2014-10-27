e4431dae3c2c62cfa6ee6917d686bbb9dba9b681
Yujin <calgaryj12@gmail.com>
2014-10-27 18:52:19 +0900

    gkgk

--- bitTest/src/bitTest/test2.java
+++ bitTest/src/bitTest/test2.java
@@ -2,6 +2,7 @@ package bitTest;
 
 public class test2 {
 public static void main(String[] args) {
-  System.out.println("동욱");
+  System.out.println("라라라라라");
+  System.out.println("동욱 하하하하");
 }
 }
